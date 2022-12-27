340229fbe8b43b086f83fb069aedd812792cdfb5
Lalit Mohanani <lalitmohanani23@gmail.com>
2022-12-27 17:16:07 +0530

    created server.js

--- starter/app.js
+++ starter/app.js
@@ -15,8 +15,5 @@ app.use(express.json());
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
 
-const port = 3000;
-app.listen(port,()=> {
-    console.log(`listening on port ${port}`);
-});
+module.exports = app;
 
